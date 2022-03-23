a06bb7f705b8314c80c90916e0439e6958451b93
Earth <pimanus1234@gmail.com>
2022-03-23 17:41:16 +0700

    Create Chef

--- modernrestaurant/src/container/Chef/Chef.jsx
+++ modernrestaurant/src/container/Chef/Chef.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
 import './Chef.css';
-
+import { images } from '../../constants'
+import { SubHeading } from '../../components';
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef"/>
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word"/>
+      <h1 className="headtext__cormorant">
+        what we believe in
+      </h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote"/>
+          <p className="p__opensans">lorem ipsum idfiosjdoigjsdfoigjosidfjgijsdofijg sdifgji sdfsosjd gdjsi</p>
+        </div>
+          <p className="p__opensans"> wekdiowjedoijcdoiwj weijroijweoijiojwe weijroiwejr wefwfwfwefwefwe</p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign"  />
+      </div>
+
+    </div>
   </div>
 );
 
